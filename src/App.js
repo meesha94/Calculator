fd7954f56c0f1a24d74b4b68ad06d5d79fb166d3
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-01-24 16:43:59 +0000

    removed file

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import './App.css';
 import Pinkcounter from './compo/counter';
 import React from 'react'
 import Calculator from './compo/calculator';
-import Todo from './compo/todo';
-import Me from './compo/todolist';
-
-
-
-
-
 
 const  App = () =>  {
   
@@ -18,8 +11,8 @@ const  App = () =>  {
       <div>
         <Pinkcounter/>
         <Calculator />
-        <Todo/>
-        <Me/>
+        
+        
         
       </div>
      
